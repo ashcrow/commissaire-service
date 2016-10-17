e72c8c4c96d83e9661a9ff0ff99d57672fe7f8be
Steve Milner <smilner@redhat.com>
2016-10-17 09:27:22 -0400

    fixup! tools: Added etcd_init.sh

--- tools/etcd_init.sh
+++ tools/etcd_init.sh
@@ -2,11 +2,17 @@
 #
 # Creates the expected etcd directories for Commissaire
 
+echo "+ Creating Commissaire keyspaces..."
 for x in clusters cluster hosts networks status; do
-  etcd_path="commissaire/"$x
-  echo "+ Creating $etcd_path"
+  etcd_path="/commissaire/"$x
+  echo "++ Creating $etcd_path"
   etcdctl mkdir $etcd_path
 done
+
+echo "+ Creating default network configuration..."
+DEFAULT_NETWORK_JSON=`python -c "from commissaire.constants import DEFAULT_CLUSTER_NETWORK_JSON; print(str(DEFAULT_CLUSTER_NETWORK_JSON).replace('\'', '\"'))"`
+etcdctl set /commissaire/networks/default "$DEFAULT_NETWORK_JSON"
+
 
 echo "+ Commissaire etcd namesapce now looks like the following:"
 etcdctl ls --recursive /commissaire/
